210d9fcce3401959b78e2fafcd69952662950861
sabahya3 <81205955+sabahya3@users.noreply.github.com>
2021-12-25 14:08:23 +0200

    Update database.php

--- database.php
+++ database.php
@@ -1,5 +1,5 @@
 <?php 
-$dsn='mysql:host=192.168.1.3;dbname=users';
+$dsn='mysql:host=127.0.0.1;dbname=users';
 // this is variable to put data to connect with database;
 // host ==> السيرفر المتصل عليه /=> ex:- host=localhost;
 // dbname ==> اسم قاعدة البيانات اللى هتصل بيها
